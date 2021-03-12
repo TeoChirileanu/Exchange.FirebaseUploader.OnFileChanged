52049c3c6e669ebaf5b67d00ee5cd898f7570747
Teodor <teodorchirileanu@gmail.com>
2021-03-12 17:46:30 +0200

    stuff i've forgotten about

---
+++ FileSync.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FileSync;
+
+Console.WriteLine("Calea catre fisierul urmarit:");
+var fileToWatch = Console.ReadLine();
+
+const string? credentialsFile = @"C:\Users\teodo\Documents\Google\TransactMe-1a88359a2131.json";
+const string? bucketName = "transactme-db.appspot.com";
+
+IFileUploader uploader = new FirebaseStorage(credentialsFile, bucketName);
+using var _ = new ReactiveFileWatcher(fileToWatch ?? throw new InvalidOperationException(), uploader.UploadFile);
+
+Console.Read();
